5e38268d0e776c960dc3587f673184414bc8b215
jakewrites <jakecahill91@gmail.com>
2018-11-06 16:17:37 +0000

    Add config manager

--- bin/app.js
+++ bin/app.js
@@ -1,2 +1,9 @@
 #! /usr/bin/env node
-console.log("Hello world");
+const CredentialManager = require('../lib/credential-manager');
+
+async function Main () {
+    const creds = new CredentialManager('wallet');
+    let [provider] = await creds.getProvider();
+    console.log(provider);
+}
+Main().catch(console.log(error));
